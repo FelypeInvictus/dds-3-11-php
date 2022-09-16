0def663a80f1113e6f75359adc42f00b3d6bd620
Felype Invictus <89306240+FelypeInvictus@users.noreply.github.com>
2022-09-16 12:05:02 -0300

    primeiro commit

---
+++ src/public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Projeto de site
+ * @author Felype Rangel <felype.sales@outlook.com>
+ * @version 1.0
+ */
+
+
+
+ include __DIR__ . "/../../vendor/autoload.php"; 
+include __DIR__ . "/../views/index.html";
